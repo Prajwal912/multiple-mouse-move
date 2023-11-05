5028cafa5cfb6af9c19f3c6f213709f971f33715
prajwal <prajwalshah912@gmail.com>
2023-11-05 23:04:37 +0530

    mid commit

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
-$(document).ready(() => {
-  let elem = $(".elem");
+// $(document).ready(() => {
+//   let elem = $(".elem");
 
-  elem.forEach(function () {
-    let val = $(this);
+//   elem.each(function (index) {
+//     let val = $(this);
 
-    val.on("mousemove", (dets) => {
-     elemIMG.css("left", dets.clientX + "px");
-     elemIMG.css("top", dets.clientY + "px");
-    });
+//     let elemIMG = val.find("img");
 
-    val.on("mouseenter", () => {
-     elemIMG.css("opacity", 1);
-     elemIMG.css("transition", "all linear 0.2s");
-    });
-    val.on("mouseleave", () => {
-     elemIMG.css("opacity", 0);
-     elemIMG.css("transition", "all linear 0.6s");
-    });
+//     val.on("mousemove", (dets) => {
+//       elemIMG.css("left", dets.clientX + "px");
+//       elemIMG.css("top", dets.clientY + "px");
+//     });
+
+//     val.on("mouseenter", () => {
+//       elemIMG.css("opacity", 1);
+//       elemIMG.css("transition", "all linear 0.2s");
+//     });
+//     val.on("mouseleave", () => {
+//       elemIMG.css("opacity", 0);
+//       elemIMG.css("transition", "all linear 0.6s");
+//     });
+//   });
+// });
+let elem = document.querySelectorAll(".elem");
+
+elem.forEach((val) => {
+  val.addEventListener("mouseenter", () => {
+    val.childNodes[3].style.opacity = 1;
+    // val.childNodes[3].style.transition = "all linear 0.2s";
   });
+  val.addEventListener("mouseenter", () => {
+    val.childNodes[3].style.opacity = 0;
+    // val.childNodes[3].style.transition = "all linear 0.6s";
+  });
+//   val.addEventListener("mouse", (dets) => {
+//     val.childNodes[3].style.left = dets.x + "px";
+//     val.childNodes[3].style.top = dets.y + "px";
+//   });
 });
